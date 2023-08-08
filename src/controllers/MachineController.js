d39ac896afc863d0e59e289f877dcd2752db8638
Giovani <giakomogcs@gmail.com>
2023-08-08 14:24:50 -0300

    Cadastrando máquina

--- src/controllers/MachineController.js
+++ src/controllers/MachineController.js
@@ -1,17 +1,29 @@
 const AppError = require("../utils/AppError")
+const sqliteConnection = require('../database/sqlite')
 
 class MachineController{
-    create(request,response){
+    async create(request,response){
       const {name} = request.body
+
+      const database = await sqliteConnection()
 
       if(!name){
         throw new AppError("Nome é obrigatório")
       }
 
+      const checkMachineExists = await database.get("SELECT * FROM machines WHERE name = (?)", [name])
+
+      if(checkMachineExists){
+        throw new AppError("Essa máquina já está em uso")
+      }
+
+      await database.run("INSERT INTO machines (name) VALUES (?)",
+      [name])
+
       response.status(201).json({name})
     }
 
-    
+
     status(request,response){
       const {name, available, working} = request.body
 
