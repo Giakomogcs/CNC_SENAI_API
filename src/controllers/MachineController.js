efd05f8db433155481049a2ef22d0f0620e55d05
Giovani <giakomogcs@gmail.com>
2023-08-08 14:13:02 -0300

    migrations machine

--- src/controllers/MachineController.js
+++ src/controllers/MachineController.js
@@ -2,6 +2,17 @@ const AppError = require("../utils/AppError")
 
 class MachineController{
     create(request,response){
+      const {name} = request.body
+
+      if(!name){
+        throw new AppError("Nome é obrigatório")
+      }
+
+      response.status(201).json({name})
+    }
+
+    
+    status(request,response){
       const {name, available, working} = request.body
 
       if(!name){
@@ -14,5 +25,7 @@ class MachineController{
       
       response.status(201).json({name, available, working})
     }
+
+
   }
   module.exports = MachineController
