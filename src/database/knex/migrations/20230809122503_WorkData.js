dcd9131db8d4661cda2ac5057cadaa6d16430cdb
Giovani <giakomogcs@gmail.com>
2023-08-09 10:03:19 -0300

    migrations

---
+++ src/database/knex/migrations/20230809122503_WorkData.js
@@ -0,0 +1,11 @@
+
+exports.up = knex => knex.schema.createTable("workdata", table => {
+    table.increments("id")
+    table.integer("machine_id").references("id").inTable("machines")
+    table.boolean("available")
+    table.boolean("working")
+    table.timestamp("timestamp").default(knex.fn.now())
+});
+  
+
+exports.down = knex => knex.schema.dropTable("workdata");
