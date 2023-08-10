5c24e8e944c806cb7d8f2ee7377c612ecacd98a1
Giovani <giakomogcs@gmail.com>
2023-08-10 09:58:24 -0300

    calculando total do turno e diferenças entre sinais

--- src/controllers/WorkDataController.js
+++ src/controllers/WorkDataController.js
@@ -1,30 +1,35 @@
 const AppError = require("../utils/AppError")
 const knex = require("../database/knex");
-const sqliteConnection = require('../database/sqlite')
-const {differenceInMinutes} = require("date-fns")
+const sqliteConnection = require('../database/sqlite');
+const moment = require("moment/moment");
 
 class WorkDataController{
     
     async create(request,response){
       const {machine_id, available, working} = request.body
-      const database = await sqliteConnection()
       
       const machine = await knex("machines").where({id:machine_id}).first()
-      //const machine = await database.get("SELECT * FROM machines WHERE id = (?)", [machine_id])
       
       if(!machine){
         throw new AppError("Não foi possível encontrar a máquina (machine_id)")
       }
 
-      const timestamp = new Date()
-
+      
       if(!machine_id){
         throw new AppError("Nome da máquina (machine_id) é obrigatório")
       }
-
+      
       if(available == working){
         throw new AppError("Não é possivel estar disponivel e trabalhando ao mesmo tempo")
       }
+
+      const zDate = new Date().toLocaleString('pt-BR', {
+        timeZone: 'America/Sao_Paulo',
+        dateStyle: 'short',
+        timeStyle: 'medium'
+      });
+
+      const timestamp = zDate
 
       await knex("workdata").insert({
         machine_id,
@@ -32,8 +37,8 @@ class WorkDataController{
         working,
         timestamp
       });
-
-  
+      
+      
       return response.json();
       
     }
@@ -41,10 +46,12 @@ class WorkDataController{
     async status(request,response){
       const{name} = request.params
       const {start, end} = request.query;
-      let datas
-      let work = []
-      let availeble = []
       
+      
+      let datas
+      let work
+      let available
+      let times = []
       
       const machine = await knex("machines").where({name}).first()
 
@@ -69,19 +76,26 @@ class WorkDataController{
         .orderBy("workdata.timestamp")
       }
       
-
+      let i=0
       datas.map(data => {
-        const timer = new Date(data.timestamp)
-        console.log(data.timestamp)
 
-        // if(data.working){
-        //   work.push(data.timestamp);
-        // }
+        var math = moment(data.timestamp, "DD/MM/YYYY HH:mm:ss").diff(moment(times[times.length-1], "DD/MM/YYYY HH:mm:ss"))
+        var mathtHours = moment.duration(math).asHours()
+
         
-        // if(data.available){
-        //   availeble.push(data.timestamp);
-        // }
+        if(i != 0){
+          console.log(`${data.timestamp} e ${times[times.length-1]}`)
+          console.log(mathtHours)
+        }
+
+        times.push(data.timestamp);
+        i++
       })
+
+      var shift = moment(times[times.length-1], "DD/MM/YYYY HH:mm:ss").diff(moment(times[0], "DD/MM/YYYY HH:mm:ss"))
+      var shiftHours = moment.duration(shift).asHours()
+
+      console.log(shiftHours)
   
       return response.json({
         datas,
